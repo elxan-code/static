cbcc4b3ecea29358b2519dde401e698c18d8a3a6
unknown <you@example.com>
2021-11-08 02:13:44 +0400

    statics

---
+++ 1/ConsoleApp1/ConsoleApp1/Extention.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Extention
+    {
+
+        public static int info(this  int c)
+        {
+            int i;
+            int a;
+            for (i = 0; i < c; i++)
+            {
+                a = i * i * i;
+                if (a == c)
+                {
+                    break;
+                }
+            }
+
+            return i;
+        }
+    }
+}
---
+++ 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int d = a.info();
+            Console.WriteLine(d);
+        }
+    }
+}
